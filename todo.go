36045803eff1fffe02916bfe7835df8f35d58640
Alex Vlasov <drumlife182@gmail.com>
2022-12-08 10:00:24 +0100

    Creating and retrieving lists

--- todo.go
+++ todo.go
@@ -1,26 +1,26 @@
 package todo
 
-type TodoList struct{
-	Id int `json:"-"`
-	Title string `json:"title"`
-	Description string `json:"description"`
+type TodoList struct {
+	Id          int    `json:"id" db:"id"`
+	Title       string `json:"title" db:"title" binding:"required"`
+	Description string `json:"description" db:"description"`
 }
 
 type UsersList struct {
-	Id int
+	Id     int
 	UserId int
 	ListId int
 }
 
 type TodoItem struct {
-	Id int `json:"id"`
-	Title string `json:"title"`
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
 	Description string `json:"description"`
-	Done bool `json:"done"`
+	Done        bool   `json:"done"`
 }
 
 type ListsItem struct {
-	Id int
+	Id     int
 	ListId int
 	ItemId int
 }
